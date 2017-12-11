ac288b46ff0ffe53cdb9086b22370eaacd97c0b5
Beomkey <21300356@handong.edu>
2017-12-12 00:45:06 +0900

    8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,18 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsq7ZJKUu4QvKY9zUTdVI2TK5PXwyxiZs",
+  authDomain: "ionic2do-3e5c1.firebaseapp.com",
+  databaseURL: "https://ionic2do-3e5c1.firebaseio.com",
+  projectId: "ionic2do-3e5c1",
+  storageBucket: "ionic2do-3e5c1.appspot.com",
+  messagingSenderId: "52330945566"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,16 +1,50 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  friends: Array<any> = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.friends = [ 
-      {name:'Jack', phone_number: '010-1111-9999', address: '포항시 북구 흥해읍 한동대학교 벧엘관'}, 
-      {name:'Bobby', phone_number: '010-3333-4444', address: '포항시 북구 흥해읍 한동대학교 은혜관'} 
-  ];
+  friends: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public af: AngularFireDatabase) {
+    this.friends = af.list('/friends');
+  }
+  add() {
+    let alert = this.alertCtrl.create({
+      title: 'Add a Member',
+      message: "Enter the member's information",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'name'
+        },
+        {
+          name: 'phone_number',
+          placeholder: 'phone_number'
+        },
+        {
+          name: 'address',
+          placeholder: 'address'
+        }        
+      ],
+      buttons: [
+        {
+          text: 'CANCEL',
+          role: 'cancel'
+        },
+        {
+          text: 'ADD',
+          handler: data => {
+            this.friends.push({name: data.name, phone_number: data.phone_number, address: data.address});
+          }
+        }
+      ]
+    })
+    alert.present();
+  }
+  delete(friend: any) {
+    this.friends.remove(friend);
   }
 }
