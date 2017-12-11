1b20b7a91cbaae18e213739abc9c434e0af7e5b5
Beomkey <21300356@handong.edu>
2017-12-11 22:55:24 +0900

    2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,15 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  members: Array<any> = [];
 
   constructor(public navCtrl: NavController) {
-
+    this.members = [
+    {name:'Father', phone_number: '010-1234-0000', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 벧엘관'}, 
+    {name:'Mother', phone_number: '010-1234-0001', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 은혜관'}, 
+    {name:'Sister', phone_number: '010-1234-0002', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 창조관'}, 
+    {name:'Brother', phone_number: '010-1234-0003', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 비전관'} 
+  ];
   }
 
 }
