0316b104abcf26d8418331caa394e32fb9f0d2c1
Beomkey <21300356@handong.edu>
2017-12-12 10:38:08 +0900

    9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+import { MemberServiceProvider } from '../providers/member-service/member-service';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -16,28 +18,30 @@ export class MyApp {
 
   pages: Array<{title: string, component: any, icon: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
+  public memberService: MemberServiceProvider) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Edit Members', component: HomePage, icon: 'create' },
-      { title: 'Friends', component: ListPage, icon: 'people' }
-    ];
+    this.getMyMembers();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
-
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+  getMyMembers(){
+    this.memberService.members$.subscribe((mems: Array<any>) => {
+      this.pages = [
+        { title: 'Edit Members', component: HomePage, icon: 'create' },
+        { title: 'Friends', component: ListPage, icon: 'people' }
+      ];
+      for (let newMem of mems) {
+        this.pages.push({ title: newMem.name, component: HomePage, icon: 'people' });
+      }
+    });
   }
 }
